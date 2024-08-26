ea4348299f37502acd60671ab9faf96d1f53d2ae
zjn_11 <1204711713@qq.com>
2024-08-26 16:05:59 +0800

    v2.2 Nacos共享配置

--- hm-api/src/main/java/com/zjn/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/zjn/api/config/DefaultFeignConfig.java
@@ -1,6 +1,9 @@
 package com.zjn.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +12,20 @@ public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLogLevel() {
         return Logger.Level.FULL;
+    }
+
+    /*
+     * OpenFeign传递用户信息
+     * */
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                Long userId = UserContext.getUser();
+                if (userId != null)
+                    requestTemplate.header("user-info", userId.toString());
+            }
+        };
     }
 }
